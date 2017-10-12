cd2f01d6e721e7012931acb3449ddbb6eb3d5e76
Wang, Wenqian <wenqianbupt@hotmail.com>
2017-10-12 23:08:57 +0100

    -

--- problems/005_the_next_palindrome.py
+++ problems/005_the_next_palindrome.py
@@ -3,7 +3,7 @@ import sys
 
 def reverse_str(value):
     """
-    >>> reverse_str("1234")
+    >>> reverse_str('1234')
     '4321'
     """
     return ''.join(list(reversed(value)))
@@ -11,6 +11,10 @@ def reverse_str(value):
 
 def get_next_palindrome(value):
     """
+    >>> get_next_palindrome('0')
+    '1'
+    >>> get_next_palindrome('10')
+    '11'
     >>> get_next_palindrome('1234')
     '1331'
     >>> get_next_palindrome('9999')
@@ -27,13 +31,44 @@ def get_next_palindrome(value):
     '1001'
     >>> get_next_palindrome('101')
     '111'
+    >>> get_next_palindrome('1')
+    '2'
+    >>> get_next_palindrome('9')
+    '11'
+    >>> get_next_palindrome('99')
+    '101'
+    >>> get_next_palindrome('94187978322')
+    '94188088149'
+    >>> get_next_palindrome('0012')
+    '22'
+    >>> get_next_palindrome('321123')
+    '322223'
+    >>> get_next_palindrome('000002133000')
+    '2133312'
+    >>> get_next_palindrome('00000213000')
+    '213312'
     """
+    value = value.lstrip('0')
+
+    if value == '':
+        return '1'
+
+    if value == '9':
+        return '11'
+
     if len(value) % 2 == 0:
         left = value[:len(value) // 2]
         right = value[len(value) // 2:]
         if left > right:
-            right = reverse_str(left)
-            return left + right
+            new_right = reverse_str(left)
+            if new_right > right:
+                return left + new_right
+            else:
+                new_left = str(int(left) + 1)
+                if len(new_left) == len(left):
+                    return new_left + reverse_str(new_left)
+                else:
+                    return new_left + '0' + reverse_str(new_left)[1:]
         else:
             new_left = str(int(left) + 1)
             if len(new_left) == len(left):
@@ -44,7 +79,12 @@ def get_next_palindrome(value):
         left = value[:len(value) // 2]
         right = value[len(value) // 2 + 1:]
         if left > right:
-            return left + value[len(value) // 2] + reverse_str(left)
+            mid = value[len(value) // 2]
+            if mid == '9':
+                new_left = str(int(left) + 1)
+                return new_left + '0' + reverse_str(new_left)
+            else:
+                return left + value[len(value) // 2] + reverse_str(left)
         elif left == right:
             mid = value[len(value) // 2]
             if mid == '9':
