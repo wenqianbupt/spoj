a75a7738ebbf82d81ce7077c0a44e0d8b673ebac
Wang, Wenqian <wenqianbupt@hotmail.com>
2017-10-13 14:39:25 +0100

    -

--- problems/002_prime_generator.py
+++ problems/002_prime_generator.py
@@ -1,14 +1,14 @@
 import sys
-
+import math
 import os
 
 def get_prime_numbers_str(start_str, stop_str):
     """
     >>> get_prime_numbers_str('2', '8')
     ['2', '3', '5', '7', '']
-    >>> get_prime_numbers_str(10, 19)
+    >>> get_prime_numbers_str('10', '19')
     ['11', '13', '17', '19', '']
-    >>> get_prime_numbers_str(18, 25)
+    >>> get_prime_numbers_str('18', '25')
     ['19', '23', '']
     """
 
@@ -19,16 +19,20 @@ def get_prime_numbers_str(start_str, stop_str):
     num = start
     while num <= stop:
         if num == 1:
-            num +=1
-        elif num == 2:
-            result.append('2')
+            num += 1
+        elif num == 2 or num == 3 or num ==5:
+            result.append(str(num))
+            num += 1
+        elif num % 2 == 0:
+            num += 1
         else:
-            for n in range(2, num):
+            for n in range(3, int(math.sqrt(num))+1):
                 if num % n == 0:
+                    num += 1
                     break
             else:
                 result.append(str(num))
-        num += 1
+                num += 1
     result.append('')
     return result
 
