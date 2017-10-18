6d1fab3a95fb44f18487cc67347e5fef34401c0c
Wang, Wenqian <wenqianbupt@hotmail.com>
2017-10-18 17:24:15 +0100

    -

--- problems/006_simple_arithmetics.py
+++ problems/006_simple_arithmetics.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import re
 
 
@@ -25,27 +26,33 @@ def fun(a, b, operator):
 def get_result(left, right, values, operator):
     right = operator + right
     max_len = max(len(left), len(right), len(values[-1]))
-    print('')
-    print(left.rjust(max_len))
-    print(right.rjust(max_len))
-    print(''.rjust(max_len, '-'))
+    result = []
+    result.append(left.rjust(max_len))
+    result.append(right.rjust(max_len))
+    length = max(len(right), len(values[0]))
+    result.append(''.ljust(length, '-').rjust(max_len, ' '))
 
     if len(values) == 1:
-        print(values[0].rjust(max_len))
+         result.append(values[0].rjust(max_len))
     else:
         for i in range(len(values) - 1):
             values[i] = values[i].ljust(int(len(values[i]) + i), ' ')
-            print(values[i].rjust(max_len))
-        print(''.rjust(max_len, '-'))
-        print(values[-1].rjust(max_len))
+            result.append(values[i].rjust(max_len))
+        result.append(''.ljust(len(values[-1]), '-').rjust(max_len, ' '))
+        result.append(values[-1].rjust(max_len))
+
+    return result
 
 if __name__ == '__main__':
     first_line = sys.stdin.readline()
     count = int(first_line)
 
-
+    results = []
 
     for i in range(count):
         s1, opr, s2 = re.split(r'(\+|\-|\*)', str(sys.stdin.readline().strip()))
         s3 = list(map(str, fun(int(s1), int(s2), opr)))
-        get_result(s1, s2, s3, opr)
+        results.extend(get_result(s1, s2, s3, opr))
+        results.append('')
+
+    print(os.linesep.join(results))
